563a3281367a0560fcb060febadda73d9e94b076
louyl <louyl@wedoctor.com>
2022-03-09 11:08:02 +0800

    update

--- app/src/components/Search/Search.tsx
+++ app/src/components/Search/Search.tsx
@@ -1,27 +1,42 @@
 import { Box, IconButton, Input } from '@mui/material';
 import classnames from 'classnames';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import s from './Search.module.css';
 import { filter } from '../../state/Filter';
 import { observer } from 'mobx-react';
+import { useEffect } from 'react';
 
 export const Search: React.FC = observer(function () {
+  // 输入框显隐状态
   const [open, toggleOpen] = useState(false);
 
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  // 当输入框显示的时候自动获得焦点
+  useEffect(() => {
+    if (open) {
+      inputRef.current?.querySelector('input')!.focus();
+    }
+  }, [open]);
+
+  // 点击搜索按钮显示输入框，并使输入框获得焦点
   const handleClick = () => {
-    toggleOpen(!open);
+    toggleOpen(true);
+    inputRef.current?.querySelector('input')!.focus();
   };
 
-  const handleBlur = () => {
-    toggleOpen(false);
+  // 当输入框失去焦点，并且没有搜索内容的时候，隐藏输入框
+  const handleBlur = (ev: any) => {
+    if (filter.keywords.trim() === '') {
+      toggleOpen(false);
+    }
   };
 
+  // 当输入框内容发生变化的时候，更新搜索内容
   const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     filter.setKeywords(event.target.value);
   };
-
-  console.log(open);
 
   return (
     <Box className={s.Search}>
@@ -31,15 +46,18 @@ export const Search: React.FC = observer(function () {
       >
         <SearchIcon fontSize="small" />
       </IconButton>
-      <Input
-        placeholder="Search..."
-        className={classnames(
-          s.Input,
-          (open || filter.keywords.trim() !== '') && s.Active
-        )}
-        onBlur={handleBlur}
-        onChange={handleInput}
-      />
+      {open ? (
+        <Input
+          ref={inputRef}
+          placeholder="Search..."
+          className={classnames(
+            s.Input,
+            (open || filter.keywords.trim() !== '') && s.Active
+          )}
+          onBlur={handleBlur}
+          onChange={handleInput}
+        />
+      ) : null}
     </Box>
   );
 });
