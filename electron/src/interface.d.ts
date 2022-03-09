7f2d3c83c7c64a835495d09ec220ca174f87fac3
louyl <louyl@wedoctor.com>
2022-03-09 18:42:17 +0800

    chore: 尝试 reference 方案

---
+++ electron/src/interface.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="../../app/src/interface.ts" />
+// import { DataRaw, Link, ElectronExpose } from '../../app/src/interface';
+
+export { DataRaw, Link, ElectronExpose };
--- electron/src/interface.ts
+++
@@ -1,4 +0,0 @@
-// <reference path="../../app/src/interface.ts" />
-import { DataRaw, Link, ElectronExpose } from '../../app/src/interface';
-
-export { DataRaw, Link, ElectronExpose };
