b25d732f4689384fa41ac13ebd4d0565ca79e90a
louyl <louyl@wedoctor.com>
2022-03-21 19:40:45 +0800

    feat: 新增分组

--- app/src/components/Groups/Groups.tsx
+++ app/src/components/Groups/Groups.tsx
@@ -3,13 +3,24 @@ import { settings } from '../../state/Settings';
 import s from './Groups.module.css';
 import classnames from 'classnames';
 import AddIcon from '@mui/icons-material/Add';
-import { useState } from 'react';
 import { IconButton } from '@mui/material';
+import { Group } from '../../interface';
+import { useEffect, useRef, useState } from 'react';
 
-const AddGroupBtton: React.FC = function () {
-  const [open, toggleOpen] = useState(false);
+const selectNodeText = function <T extends Node = HTMLElement>(node: T): void {
+  const selection = window.getSelection();
+  const range = document.createRange();
+  range.selectNodeContents(node);
+  selection?.removeAllRanges();
+  selection?.addRange(range);
+};
+
+const AddGroupBtton: React.FC<{
+  editGroup?: (addedGroup: Group) => void;
+}> = function (props) {
   const handleClick = () => {
-    toggleOpen(!open);
+    const addedGroup = settings.addGroup('未命名');
+    props.editGroup?.(addedGroup);
   };
 
   return (
@@ -23,6 +34,40 @@ const AddGroupBtton: React.FC = function () {
 
 export const Groups: React.FC = observer(function () {
   const { activatedGroupId, groups } = settings;
+  const [editing, setEditing] = useState<null | Group>(null);
+  const inputEl = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputEl.current) {
+      inputEl.current.focus();
+      selectNodeText(inputEl.current);
+    }
+  }, [editing?.id]);
+
+  const editGroup = (group: Group) => {
+    setEditing(group);
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      rename(e.currentTarget.innerText);
+    } else if (e.key === 'Escape') {
+      e.preventDefault();
+      rename(editing!.name);
+    }
+  };
+
+  const onBlur = (e: React.FocusEvent<HTMLDivElement>) => {
+    rename(e.currentTarget.innerText);
+  };
+
+  const rename = (name: string) => {
+    name = name.trim() || '未命名';
+    settings.renameGroup(editing!.id, name);
+    inputEl.current!.innerText = editing!.name;
+    setEditing(null);
+  };
 
   return (
     <div className={s.Groups}>
@@ -32,12 +77,19 @@ export const Groups: React.FC = observer(function () {
             [s.Active]: group.id === activatedGroupId,
           })}
           key={group.id}
+          data-key={group.id}
           onClick={() => settings.activeGroup(group.id)}
+          ref={editing?.id === group.id ? inputEl : null}
+          suppressContentEditableWarning={true}
+          contentEditable={editing?.id === group.id}
+          onKeyDown={onKeyDown}
+          onBlur={onBlur}
         >
           {group.name}
         </div>
       ))}
-      <AddGroupBtton />
+
+      <AddGroupBtton editGroup={(group) => editGroup(group)} />
     </div>
   );
 });
