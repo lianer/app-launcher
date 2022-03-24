75c25968d66666484bdac81adf12933aa4086b40
louyl <louyl@wedoctor.com>
2022-03-24 17:23:13 +0800

    feat: 调整可拖拽窗口的区域

--- app/src/components/MenuBar/MenuBar.tsx
+++ app/src/components/MenuBar/MenuBar.tsx
@@ -6,9 +6,14 @@ import { Search } from '../Search';
 
 // TODO [ ] 调整可拖拽窗口的范围
 
+const DragableBar: React.FC = function () {
+  return <div className={s.DragableBar}></div>;
+};
+
 export const MenuBar: React.FC = function () {
   return (
     <Box className={s.MenuBar}>
+      <DragableBar />
       <Search />
       <Groups />
       <More />
