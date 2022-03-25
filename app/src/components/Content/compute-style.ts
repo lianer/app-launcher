47fc885717828a79d14a1ad62c9edd2f47cc2b96
louyl <louyl@wedoctor.com>
2022-03-25 20:03:10 +0800

    feat: 优化 content links 布局样式

--- app/src/components/Content/compute-style.ts
+++ app/src/components/Content/compute-style.ts
@@ -2,9 +2,9 @@ export const computeStyle = function (iconSize: number) {
   const style = {
     size: iconSize,
     margin: iconSize / 10,
-    padding: iconSize / 10,
+    padding: iconSize / 5,
     titleHeight: iconSize / 2,
-    titleFontSize: (iconSize / 2) * (3 / 8), // 字体大小为标题高度的 3/8，在图标为 64px 的情况下，刚好获得 12px 的字号
+    titleFontSize: (iconSize / 64) * 14, // 默认 64px 图标对应 14px 字号
   };
 
   const scrollerStyle = {
@@ -21,8 +21,7 @@ export const computeStyle = function (iconSize: number) {
   const linkStyle = {
     width: style.size + style.padding * 2, // padding-left/padding-right 集成到 width 中
     height: style.size + style.titleHeight,
-    paddingTop: style.padding,
-    paddingBottom: style.padding,
+    padding: style.padding,
   };
 
   const iconStyle = {
@@ -31,7 +30,7 @@ export const computeStyle = function (iconSize: number) {
   };
 
   const titleStyle = {
-    width: style.size + style.padding, // 只加一个 padding，另一个 padding 留一点边距好看一些
+    width: style.size + style.padding * 3, // 只加一个 padding，另一个 padding 留一点边距好看一些
     height: style.titleHeight,
     lineHeight: `${style.titleHeight}px`,
     fontSize: style.titleFontSize,
